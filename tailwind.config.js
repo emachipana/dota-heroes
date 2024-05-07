bdf1a666c808bb1931228cf62508d9e85ae566a6
Enmanuel Chipana <emachipana0102@gmail.com>
2024-05-07 09:38:20 -0500

    ADD: custom classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,14 @@ module.exports = {
         "blue-custom": "#223F59"
       },
       fontFamily: {
-        "nunito": ["Nunito", "sans-serif"]
+        "nunito": ["Nunito", "sans-serif"],
+        "odor": ["Odor Mean Chey", "serif"]
+      },
+      height: {
+        "hero": "calc(100vh - 180px)",
+      },
+      borderWidth: {
+        "3": "3px"
       }
     },
   },
