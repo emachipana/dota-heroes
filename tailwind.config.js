7a0abbcd300126dae4b405812a47a3fcc2650f73
Enmanuel Chipana <emachipana0102@gmail.com>
2024-05-07 19:34:25 -0500

    ADD: custom classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,14 +12,20 @@ module.exports = {
         "blue-custom": "#223F59"
       },
       fontFamily: {
-        "nunito": ["Nunito", "sans-serif"],
-        "odor": ["Odor Mean Chey", "serif"]
+        nunito: ["Nunito", "sans-serif"],
+        odor: ["Odor Mean Chey", "serif"]
       },
       height: {
-        "hero": "calc(100vh - 180px)",
+        hero: "calc(100vh - 180px)",
       },
       borderWidth: {
-        "3": "3px"
+        3: "3px"
+      },
+      boxShadow: {
+        custom: "0px 0px 10px 5px rgba(0, 0, 0, .5)"
+      },
+      scale: {
+        115: "scale(1.15)"
       }
     },
   },
